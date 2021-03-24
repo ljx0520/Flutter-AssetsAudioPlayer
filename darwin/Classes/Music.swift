6d0fe95cc13e4f0737468607e29a7450f1654a6d
josephli <ljx930520@gmail.com>
2021-03-24 14:55:54 +1100

    seek check if playing

--- darwin/Classes/Music.swift
+++ darwin/Classes/Music.swift
@@ -762,13 +762,10 @@ public class Player : NSObject, AVAudioPlayerDelegate {
     }
     
     func seek(to: Int){
-        // let isPlaying = self.playing
-        // self.pause()
+        self.pause()
         let targetTime = CMTimeMakeWithSeconds(Double(to) / 1000.0, preferredTimescale: 1000000)
         self.player?.seek(to: targetTime, toleranceBefore: .zero, toleranceAfter: .zero)
-        // if(isPlaying){
-        //     self.play()
-        // }
+        self.play()
     }
 
     func setVolume(volume: Double){
