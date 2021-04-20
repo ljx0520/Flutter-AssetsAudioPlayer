3520ead9bda74ab0d4c29f21cbf54100dce1871e
josephli <ljx930520@gmail.com>
2021-04-20 15:11:32 +1000

    null safety

--- android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationActionReceiver.kt
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationActionReceiver.kt
@@ -11,13 +11,13 @@ class NotificationActionReceiver : BroadcastReceiver() {
         val trackID = if (intent.getStringExtra(NotificationService.TRACK_ID) == null) "" else intent.getStringExtra(NotificationService.TRACK_ID)
         val player = AssetsAudioPlayerPlugin.instance?.assetsAudioPlayer?.getPlayer(playerId) ?: return
         when (intent.action) {
-             NotificationAction.ACTION_PREV -> player.prev()
-             NotificationAction.ACTION_STOP -> {
-                 player.askStop()
-                 //NotificationManager(context).hideNotification()
-             }
-             NotificationAction.ACTION_NEXT -> player.next()
-             NotificationAction.ACTION_TOGGLE -> {
+            NotificationAction.ACTION_PREV -> player.prev()
+            NotificationAction.ACTION_STOP -> {
+                player.askStop()
+                //NotificationManager(context).hideNotification()
+            }
+            NotificationAction.ACTION_NEXT -> player.next()
+            NotificationAction.ACTION_TOGGLE -> {
                 //val notificationAction = intent.getSerializableExtra(NotificationService.EXTRA_NOTIFICATION_ACTION) as NotificationAction.Show
                 player.askPlayOrPause() //send it to flutter
 
