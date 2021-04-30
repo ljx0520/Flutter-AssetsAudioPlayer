fa61d27734236eb7ff7b9b6ec0c87a21fc28ccaa
josephli <ljx930520@gmail.com>
2021-04-30 20:00:12 +1000

    fixed flashing seek

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,20 +35,22 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final audios = <Audio>[
-    //Audio.network(
-    //  'https://d14nt81hc5bide.cloudfront.net/U7ZRzzHfk8pvmW28sziKKPzK',
-    //  metas: Metas(
-    //    id: 'Invalid',
-    //    title: 'Invalid',
-    //    artist: 'Florent Champigny',
-    //    album: 'OnlineAlbum',
-    //    image: MetasImage.network(
-    //        'https://image.shutterstock.com/image-vector/pop-music-text-art-colorful-600w-515538502.jpg'),
-    //  ),
-    //),
     Audio.network(
       // 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Music_for_Video/springtide/Sounds_strange_weird_but_unmistakably_romantic_Vol1/springtide_-_03_-_We_Are_Heading_to_the_East.mp3',
-      'https://api.muslimlife.com.au/HLS/QqRTSSRnMah7jLdhGKEMdJ/cfe9204ca3bbf7354f757eab4c738061.m3u8',
+      'https://d3htvxjlqehrhp.cloudfront.net/HLS/QGuoy8ViisssY5aRRx4QsC/291bd4f95ba57a1422078484e5453dc9.m3u8',
+      metas: Metas(
+        id: 'Chapter 18',
+        title: 'Online',
+        artist: 'Florent Champigny',
+        album: 'OnlineAlbum',
+        // image: MetasImage.network('https://www.google.com')
+        image: MetasImage.network(
+            'https://image.shutterstock.com/image-vector/pop-music-text-art-colorful-600w-515538502.jpg'),
+      ),
+    ),
+    Audio.network(
+      // 'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/Music_for_Video/springtide/Sounds_strange_weird_but_unmistakably_romantic_Vol1/springtide_-_03_-_We_Are_Heading_to_the_East.mp3',
+      'https://d3htvxjlqehrhp.cloudfront.net/HLS/dMEnVZGXhvFeehUTfpnzeT/c5641c41ffc7f9d03005a8380ecc21bb.m3u8',
       metas: Metas(
         id: 'Online',
         title: 'Online',
@@ -59,72 +61,7 @@ class _MyAppState extends State<MyApp> {
             'https://image.shutterstock.com/image-vector/pop-music-text-art-colorful-600w-515538502.jpg'),
       ),
     ),
-    Audio(
-      'assets/audios/rock.mp3',
-      //playSpeed: 2.0,
-      metas: Metas(
-        id: 'Rock',
-        title: 'Rock',
-        artist: 'Florent Champigny',
-        album: 'RockAlbum',
-        image: MetasImage.network(
-            'https://static.radio.fr/images/broadcasts/cb/ef/2075/c300.png'),
-      ),
-    ),
-    Audio(
-      'assets/audios/2 country.mp3',
-      metas: Metas(
-        id: 'Country',
-        title: 'Country',
-        artist: 'Florent Champigny',
-        album: 'CountryAlbum',
-        image: MetasImage.asset('assets/images/country.jpg'),
-      ),
-    ),
-    Audio(
-      'assets/audios/electronic.mp3',
-      metas: Metas(
-        id: 'Electronics',
-        title: 'Electronic',
-        artist: 'Florent Champigny',
-        album: 'ElectronicAlbum',
-        image: MetasImage.network(
-            'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg'),
-      ),
-    ),
-    Audio(
-      'assets/audios/hiphop.mp3',
-      metas: Metas(
-        id: 'Hiphop',
-        title: 'HipHop',
-        artist: 'Florent Champigny',
-        album: 'HipHopAlbum',
-        image: MetasImage.network(
-            'https://beyoudancestudio.ch/wp-content/uploads/2019/01/apprendre-danser.hiphop-1.jpg'),
-      ),
-    ),
-    Audio(
-      'assets/audios/pop.mp3',
-      metas: Metas(
-        id: 'Pop',
-        title: 'Pop',
-        artist: 'Florent Champigny',
-        album: 'PopAlbum',
-        image: MetasImage.network(
-            'https://image.shutterstock.com/image-vector/pop-music-text-art-colorful-600w-515538502.jpg'),
-      ),
-    ),
-    Audio(
-      'assets/audios/instrumental.mp3',
-      metas: Metas(
-        id: 'Instrumental',
-        title: 'Instrumental',
-        artist: 'Florent Champigny',
-        album: 'InstrumentalAlbum',
-        image: MetasImage.network(
-            'https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/attachment_68585523.jpg'),
-      ),
-    ),
+
   ];
 
   //final AssetsAudioPlayer _assetsAudioPlayer = AssetsAudioPlayer();
@@ -150,12 +87,15 @@ class _MyAppState extends State<MyApp> {
     }));
   }
 
-  void openPlayer() async {
+   void openPlayer() async {
     await _assetsAudioPlayer.open(
       Playlist(audios: audios, startIndex: 0),
       showNotification: true,
       autoStart: false,
     );
+
+    _assetsAudioPlayer.seek(Duration(seconds: 40));
+    _assetsAudioPlayer.play();
   }
 
   @override
